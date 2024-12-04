88ca4f6009cf5b5d5a521bc2ec1f4e106a7a6c2b
Maureen Larkin <maureenlarkin55@gmail.com>
2024-12-03 22:28:46 -0800

    committing updated blogging functionality

--- blogging/views.py
+++ blogging/views.py
@@ -19,18 +19,27 @@ def stub_view(request, *args, **kwargs):
 '''
 Pre re-write with django builtin render view
 '''
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
-
-# rewrite our view
 # def list_view(request):
 #     published = Post.objects.exclude(published_date__exact=None)
 #     posts = published.order_by('-published_date')
+#     template = loader.get_template('blogging/list.html')
 #     context = {'posts': posts}
-#     return render(request, 'list.html', context)
+#     body = template.render(context)
+#     return HttpResponse(body, content_type="text/html")
+
+# rewrite our view
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
 
