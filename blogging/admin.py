71e8569cf1576b483561c889754109a353500d7a
Maureen Larkin <maureenlarkin55@gmail.com>
2024-12-05 21:11:19 -0800

    Updated admin interface for blogging posts and categories

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
 from blogging.models import Post, Category
-
-admin.site.register(Post)
-admin.site.register(Category)
+# from myproject.admin_site import custom_admin_site
 
 # Register your models here.
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+        ]
+
+admin.site.register(Post, PostAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+admin.site.register(Category, CategoryAdmin)
+
+
