5d9343834928c5b5c9f25a1d6a5c941eef6d2002
Maureen Larkin <maureenlarkin55@gmail.com>
2024-12-16 21:12:09 -0800

    Completed Lesson 8

---
+++ blogging/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from blogging.models import Post, Category
+# from myproject.admin_site import custom_admin_site
+
+# Register your models here.
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+        ]
+
+admin.site.register(Post, PostAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+admin.site.register(Category, CategoryAdmin)
+
+
---
+++ blogging/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from blogging.models import Post
+
+# Create your views here.
+
+'''
+Commented out to create a class-based view
+# rewrite our view:
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
+'''
+
+class PostListView(ListView):
+    # model = Post
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date') # apply both 'filter' and 'order_by' methods to 'Post.objects' query set
+    template_name = 'blogging/list.html'
+
+'''
+Commented out to make a class-based view
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
+'''
+class PostDetailView(DetailView):
+    # model = Post
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
+
+
