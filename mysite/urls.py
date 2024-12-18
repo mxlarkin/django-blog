c3d4df12da65083cffb6901d837364c40439bac5
Maureen Larkin <maureenlarkin55@gmail.com>
2024-12-18 01:04:35 -0800

    Added login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView  # new
 
 urlpatterns = [
-    path("", include("blogging.urls")),
+    path("", include("blogging.urls")),  # maybe take this out?
     path("polling/", include("polling.urls")),
     path("admin/", admin.site.urls),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    path("accounts/", include("allauth.urls")),  # new
+    path("", TemplateView.as_view(template_name="home.html")),  # new
 ]
